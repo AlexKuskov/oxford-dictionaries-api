85f3e8a7b33170d1d65e5ae6fb44293a4ec5fc3e
FalseNumber <sasha.sanya.aleksandr@gmail.com>
2019-08-04 23:25:15 +0300

    Add displaying word api data for word-info component

--- src/app/components/content/word-info/word-info.component.ts
+++ src/app/components/content/word-info/word-info.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { MainService } from 'src/app/sevices/main.service';
 import { Root } from 'src/app/models/root';
 import { Result } from 'src/app/models/result';
+import { Pronunciation } from 'src/app/models/pronunciation';
+import { LexicalEntry } from 'src/app/models/lexical-entry';
+import { Sense } from 'src/app/models/sense';
+import { Subsense } from 'src/app/models/subsense';
 
 @Component({
   selector: 'app-word-info',
@@ -12,7 +16,12 @@ export class WordInfoComponent implements OnInit {
 
   word: string = 'dd';
   result: Root;
+  
   results: Result[] = [];
+  lexicalEntries: LexicalEntry[] = [];
+  senses: Sense[] = [];
+  subsenses: Subsense[] = [];
+  pronunciation: Pronunciation;
 
   constructor(private mainService: MainService) { }
 
@@ -23,10 +32,14 @@ export class WordInfoComponent implements OnInit {
     this.mainService.getResult().subscribe(response => {
       console.log("word:");
       console.log(response.id);
+      this.word = response.id;
       
       console.log("audio:");
-      console.log(response.results[0].lexicalEntries[0].pronunciations[0].audioFile);
-      console.log(response.results[0].lexicalEntries[0].pronunciations[0].phoneticSpelling);
+      this.results = response.results;
+
+      this.pronunciation = response.results[0].lexicalEntries[0].pronunciations[0];
+      console.log(this.pronunciation.audioFile);
+      console.log(this.pronunciation.phoneticSpelling);
       
       console.log("noun:");
       console.log(response.results[0].lexicalEntries[0].lexicalCategory.id);
@@ -45,6 +58,26 @@ export class WordInfoComponent implements OnInit {
       console.log("origin:");
       console.log(response.results[0].lexicalEntries[0].entries[0].etymologies[0]);
     });
+  }
+
+  editDescription(description: string) {
+    return this.capitalizeFirstLetter(this.addDot(description));
+  }
+
+  addDot(description: string): string {
+    return description.charAt(description.length - 1) === '.' ? description : description + '.';
+  }
+
+  capitalizeFirstLetter(description: string) {
+    return description.charAt(0).toUpperCase() + description.slice(1);
+  }
+
+  isWordFormNoteType(noteType: string): boolean {
+    return noteType === 'wordFormNote';
+  }
+
+  isGrammaticalNoteType(noteType: string): boolean {
+    return noteType === 'grammaticalNote';
   }
 
 }
